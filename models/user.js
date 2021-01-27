1893d54102b713b07c13a1ba9eee6d6d7a00a1e0
Kevin Cho <kccho2254@gmail.com>
2021-01-26 21:27:18 -0600

    basic login/logout works from localhost; set passwords in config.json to null so you will need to set your own password. added sequelize-cli to make stuff work. To start the app you need to run the command $ sequelize db:create in order to automatically create databases. Commented out everything in .travis.yml, hopefully it won't flag eslint problems to stop us from pushing up to main.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
-  const User = sequelize.define("User", {
+  const User = sequelize.define('User', {
     // The email cannot be null, and must be a proper email before creation
     email: {
       type: DataTypes.STRING,
@@ -24,7 +24,7 @@ module.exports = function(sequelize, DataTypes) {
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", user => {
+  User.addHook('beforeCreate', user => {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
