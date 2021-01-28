aee26df11e570ac57841185852a5070e2777f6d7
Charli <charlispilecki@gmail.com>
2021-01-27 20:59:16 -0600

    Fixed bug in sign up page

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     estimatedTotalCost: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: true
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
