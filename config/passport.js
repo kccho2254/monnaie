630ccb67bb172bc349aa28255a7ba33d6258a4e2
Kevin Cho <kccho2254@gmail.com>
2021-01-27 19:35:29 -0600

    added env/dependency, must make a .env file with the following: 'DB_USER = [your username] DB_PASS = [your password]'

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 const bcrypt = require('bcryptjs');
 // Creating our User model
