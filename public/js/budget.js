e91b5fd6e32d4b99ea4ba543ec7ea7ab29d2d7a7
Charli <charlispilecki@gmail.com>
2021-01-29 00:06:49 -0600

    Added a function for budget sums
    Wrote code to get the sum of budget line items for total estimated cost. Created a test html page to add dinero

---
+++ public/js/budget.js
@@ -0,0 +1,33 @@
+function calculateEstimatedTotal(budget_categories) {
+    let estimatedTotal = Dinero({ amount: 0 })
+
+    budget_categories.forEach(category => {
+        category.budget_line_items.forEach(line_item => {
+            let estimated_cost = Dinero({ amount: line_item.estimated_cost })
+            estimatedTotal = estimatedTotal.add(estimated_cost)
+        })
+    })
+    
+    return estimatedTotal
+}
+
+let budget_categories = [
+    {
+        desc: "Venue",
+        budget_line_items: [
+            {
+                desc: "Hummingbird House",
+                estimated_cost: 480000,
+                actual_cost: 0
+            },
+            {
+                desc: "Parking fee",
+                estimated_cost: 8050,
+                actual_cost: 0
+            }
+        ]
+    }
+]
+
+let estimatedTotal = calculateEstimatedTotal(budget_categories)
+alert("Calculated estimated total: " + estimatedTotal.toFormat('$0,0.00'))
